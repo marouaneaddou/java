c37a4893bafed457e134501730ba76d0ac19b1e9
Marouane Addou <105064179+marouaneaddou@users.noreply.github.com>
2026-03-16 16:46:31 +0000

    String contains method

---
+++ java-intro/StringContains/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(StringContains.isStringContainedIn("Hell", "Highway to Hell"));
+        System.out.println(StringContains.isStringContainedIn("Hell", "Hello World !"));
+        System.out.println(StringContains.isStringContainedIn("Bonjour", "hello World !"));
+    }
+}
