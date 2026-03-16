449d2f79b627d297503f7dfcf7e8b934a16b7152
Marouane Addou <105064179+marouaneaddou@users.noreply.github.com>
2026-03-16 18:24:21 +0000

    Add null handling to string contains method

--- java-intro/StringContains/Test.java
+++ java-intro/StringContains/Test.java
@@ -2,6 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println(StringContains.isStringContainedIn("Hell", "Highway to Hell"));
         System.out.println(StringContains.isStringContainedIn("Hell", "Hello World !"));
-        System.out.println(StringContains.isStringContainedIn("Bonjour", "hello World !"));
+        System.out.println(StringContains.isStringContainedIn(null, "hello World !"));
     }
 }
