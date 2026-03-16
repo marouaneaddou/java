d5e08ba7195cc845ef9fa61d0e54f46a72c146af
Marouane Addou <105064179+marouaneaddou@users.noreply.github.com>
2026-03-16 18:13:25 +0000

    Add null handling to replace method

--- java-intro/StringReplace/Test.java
+++ java-intro/StringReplace/Test.java
@@ -1,7 +1,9 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println(StringReplace.replace("javatpoint is a very good website", 'a', 'e'));
+        System.out.println(StringReplace.replace(null, 'a', 'e'));
         System.out.println(StringReplace.replace("my name is khan my name is java", "is","was"));
         System.out.println(StringReplace.replace("hey i'am java", "I'am","was"));
+        System.out.println(StringReplace.replace("hey i'am java", null,"was"));
     }
 }
