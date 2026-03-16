8fcbf06a729f87661dcbf35f6415a3584fa43790
Marouane Addou <105064179+marouaneaddou@users.noreply.github.com>
2026-03-16 16:33:10 +0000

    String length

---
+++ java-intro/StringLength/Test.java
@@ -0,0 +1,6 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(StringLength.getStringLength("Hello World !"));
+        System.out.println(StringLength.getStringLength(""));
+    }
+}
